7d524388fd7733498d4a7f9bf54f8ca5d056cbba
German Novikov <german.novikov@phystech.edu>
2020-04-08 00:40:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,9 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("TOKEN", use_context=True, request_kwargs=json.load(open("proxy.json")))
+    updater = Updater("1138603216:AAEgCb-4q_yMBTmeF993hTafe9-FD3bO0pE", 
+                      use_context=True, 
+                      request_kwargs=json.load(open("proxy.json")))
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
